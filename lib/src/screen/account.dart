20c5ec3aabe0933d4bae0c1c0cf628200c503f57
SubrataChk <subrataclash@gmail.com>
2022-05-01 20:28:36 +0600

    master

--- lib/src/screen/account.dart
+++ lib/src/screen/account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peer_vendors/src/app/app_color.dart';
+import 'package:sizer/sizer.dart';
 
 class MyAccountSection extends StatefulWidget {
   const MyAccountSection({Key? key}) : super(key: key);
@@ -10,6 +12,54 @@ class MyAccountSection extends StatefulWidget {
 class _MyAccountSectionState extends State<MyAccountSection> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.btnColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "Settings",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 18.sp),
+        ),
+      ),
+      body: SafeArea(
+          child: ListView(
+        children: [
+          CustomListTiles(title: "Edit Profile"),
+          CustomListTiles(title: "Language"),
+          CustomListTiles(title: "log out"),
+          CustomListTiles(title: "Deativate Profile"),
+        ],
+      )),
+    );
+  }
+}
+
+class CustomListTiles extends StatelessWidget {
+  final String title;
+  const CustomListTiles({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: ListTile(
+          leading: CircleAvatar(radius: 4.w, backgroundColor: Colors.grey),
+          title: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15.sp),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/screen/chat.dart
+++ lib/src/screen/chat.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
@@ -24,28 +25,78 @@ class _MyChatSectionState extends State<MyChatSection> {
           ),
         ),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 6.w),
+          padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 2.h),
           child: Row(
             children: [
               Expanded(
                 flex: 4,
-                child: TextFormField(),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(16),
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 7.w,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                ),
               ),
               SizedBox(
                 width: 3.w,
               ),
               Expanded(
-                // flex: 1,
+                flex: 1,
                 child: Container(
-                  height: 30,
-                  width: 20,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.blueAccent),
-                ),
+                    height: 5.h,
+                    width: 4.w,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.add))),
               )
             ],
           ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundColor: Colors.white,
+                        backgroundImage: NetworkImage(
+                            "https://www.varietyinsight.com/images/honoree/Tom_Cruise.png"),
+                      ),
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bozenka Malina",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14.sp,
+                              ),
+                            ),
+                            Text(
+                              "Uploaded File",
+                              style: TextStyle(
+                                  fontSize: 12.sp, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
+                );
+              }),
         )
       ]),
     );
